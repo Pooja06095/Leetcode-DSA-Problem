1bc33440e68348c4c0e2498a1af09dd07fd9bb00
Pooja Goswami <apoojagoswami5@gmail.com>
2026-09-03 10:43:26 +0530

    Add Two Sum solution

---
+++ 1_Two_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+
+# Testing
+nums = [2, 7, 11, 15]
+target = 9
+
+solution = Solution()
+print(solution.twoSum(nums, target))
