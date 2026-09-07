9cf4148eb74b5d29e155fc89d2fedc2574920df6
Pooja Goswami <apoojagoswami5@gmail.com>
2026-09-07 19:15:38 +0530

    Solved Counting Bits

---
+++ 338_CountingBits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBits(self, n: int) -> list[int]:
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+
+        return ans
+
+
+solution = Solution()
+
+print(solution.countBits(2))
+print(solution.countBits(5))
