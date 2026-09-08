6efde7407dcfd4ef94071018572bfc1f3ea4db59
Pooja Goswami <apoojagoswami5@gmail.com>
2026-09-08 21:50:40 +0530

    Solved Minimum Bit Flips to Convert Number

---
+++ 2220_MinimumBitFlipsToConvertNumber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        xor = start ^ goal
+        count = 0
+
+        while xor > 0:
+            count += xor & 1
+            xor >>= 1
+
+        return count
+
+
+solution = Solution()
+
+print(solution.minBitFlips(10, 7))
+print(solution.minBitFlips(3, 4))
