0271289d9051260b1a6f2786075bf652702ec0dd
Pooja Goswami <apoojagoswami5@gmail.com>
2026-09-14 13:18:47 +0530

    Solve LeetCode 205 Isomorphic Strings

---
+++ 205_Isomorphic_Strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        map_s_t = {}
+        map_t_s = {}
+
+        for a, b in zip(s, t):
+
+            if a in map_s_t and map_s_t[a] != b:
+                return False
+
+            if b in map_t_s and map_t_s[b] != a:
+                return False
+
+            map_s_t[a] = b
+            map_t_s[b] = a
+
+        return True
