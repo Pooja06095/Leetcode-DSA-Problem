45d6913bbe7d8655f0939f5bce3f11636731eb1d
Pooja Goswami <apoojagoswami5@gmail.com>
2026-09-26 21:05:38 +0530

    Solve LeetCode 133 Clone Graph

---
+++ 133_Clone_Graph.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def cloneGraph(self, node):
+        if node is None:
+            return None
+
+        clones = {node: Node(node.val)}
+        queue = deque([node])
+
+        while queue:
+            current = queue.popleft()
+
+            for neighbor in current.neighbors:
+                if neighbor not in clones:
+                    clones[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+
+                clones[current].neighbors.append(clones[neighbor])
+
+        return clones[node]
